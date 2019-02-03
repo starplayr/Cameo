376e9565b39df11e027ca7bf3ada842a7089bfeb
Todd Bruss <brusstodd@gmail.com>
2019-02-03 11:17:33 -0500

    remove 5 lines of code releated to status.

--- Sources/Cameo/DavidHasselhoff/PostSync.swift
+++ Sources/Cameo/DavidHasselhoff/PostSync.swift
@@ -8,13 +8,13 @@
 
 import Foundation
 
-typealias ReturnTuple = (message: String, success: Bool, data: Dictionary<String, Any>, response: HTTPURLResponse )
+typealias PostReturnTuple = (message: String, success: Bool, data: Dictionary<String, Any>, response: HTTPURLResponse )
 
-internal func PostSync(request: Dictionary<String, Any>, endpoint: String, method: String) -> ReturnTuple  {
+internal func PostSync(request: Dictionary<String, Any>, endpoint: String, method: String) -> PostReturnTuple  {
     
     //MARK - for Sync
     let semaphore = DispatchSemaphore(value: 0)
-    var syncData : ReturnTuple? = (message: "", success: false, data: Dictionary<String, Any>(), response: HTTPURLResponse() )
+    var syncData : PostReturnTuple? = (message: "", success: false, data: Dictionary<String, Any>(), response: HTTPURLResponse() )
     let http_method = "POST"
     let time_out = 30
     let url = URL(string: endpoint)
@@ -28,31 +28,25 @@ internal func PostSync(request: Dictionary<String, Any>, endpoint: String, metho
         urlReq!.setValue("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15", forHTTPHeaderField: "User-Agent")
         let task = URLSession.shared.dataTask(with: urlReq! ) { ( rData, resp, error ) in
             
-            var status = 400
-            
-            if resp != nil {
-                let r = resp as! HTTPURLResponse
-                status = r.statusCode
-            }
-            
-            if status == 200 {
+            if (resp as! HTTPURLResponse).statusCode == 200 {
                 
                 do { let result =
                     try JSONSerialization.jsonObject(with: rData!, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : Any]
                     syncData = (message: method + " was successful.", success: true, data: result, response: resp as! HTTPURLResponse )
                 } catch {
                     //fail on any errors
-                    syncData = (message: method + " failed in do try catch.", success: false, data: ["Error": "Catch Error"], response: resp as! HTTPURLResponse )
+                    syncData = (message: method + " failed in do try catch.", success: false, data: ["": ""], response: resp as! HTTPURLResponse )
                 }
             } else {
                 //we always require 200 on the post, anything else is a failure
                 
                 if resp != nil {
-                    syncData = (message: method + " failed!", success: false, data: ["Error": "Fatal Error"], response: resp as! HTTPURLResponse )
+                    syncData = (message: method + " failed, see response.", success: false, data: ["": ""], response: resp as! HTTPURLResponse ) as PostReturnTuple
                 } else {
-                    syncData = (message: method + " failed!", success: false, data: ["Error": "Fatal Error"], response: HTTPURLResponse() )
+                    syncData = (message: method + " failed, no response.", success: false, data: ["": ""], response: HTTPURLResponse() ) as PostReturnTuple
                 }
             }
+            
             //MARK - for Sync
             semaphore.signal()
         }
@@ -67,7 +61,7 @@ internal func PostSync(request: Dictionary<String, Any>, endpoint: String, metho
         return syncData!
     }
     
-    return (message: method + " failed!", success: false, data: ["Error": "Fatal Error"], response: HTTPURLResponse() )
+    return (message: method + " failed!", success: false, data: ["Error": "Fatal Error"], response: HTTPURLResponse() ) as PostReturnTuple
 }
 
 
