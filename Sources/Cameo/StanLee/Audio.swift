68827b93563272def57346fee8272b23ba1d7d3d
Todd Bruss <brusstodd@gmail.com>
2019-02-04 21:09:08 -0500

    minor update

--- Sources/Cameo/StanLee/Audio.swift
+++ Sources/Cameo/StanLee/Audio.swift
@@ -5,10 +5,6 @@ func Audio(data: String, channelId: String, userid: String) -> Data {
     
     let prefix = Global.variable.hls_sources["Live_Primary_HLS"]! + "/AAC_Data/" + channelId + "/HLS_" + channelId + "_256k_v3/"
     let suffix = Global.variable.user[userid]!.consumer  + "&token=" + Global.variable.user[userid]!.token
-    
-    //playlist = playlist!.replacingOccurrences(of: channelId, with: prefix + channelId)
-    //playlist = playlist!.replacingOccurrences(of: ".aac", with: ".aac" + suffix)
-    
     let endpoint = prefix + data + suffix
     let audio = DataSync(endpoint: endpoint, method: "AAC")
 
